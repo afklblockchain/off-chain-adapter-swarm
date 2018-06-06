10ac5b1b77af4e90fd39fd6a8ca3ac10bad4bb30
Hynek Urban <hynek.urban@fragaria.cz>
2018-06-06 11:37:24 +0200

    Fix the local swarm script so that it works in Travis.

--- test/scripts/localswarm.sh
+++ test/scripts/localswarm.sh
@@ -8,23 +8,39 @@ if [ $? -eq 0 ]; then
     exit 0;
 fi
 
+BASE_DIR=`pwd`/swarm_data
 GETH_VERSION="1.8.10-eae63c51"
-BIN_DIR=/tmp/geth/geth-alltools-linux-386-$GETH_VERSION
+ARCHITECTURE=amd64
+BIN_DIR=$BASE_DIR/geth-alltools-linux-$ARCHITECTURE-$GETH_VERSION
 
 # Download the swarm binary if necessary.
 if [ ! -d $BIN_DIR ]; then
-    mkdir -p /tmp/geth
-    wget https://gethstore.blob.core.windows.net/builds/geth-alltools-linux-386-$GETH_VERSION.tar.gz -O /tmp/geth/geth.tar.gz
-    tar --directory /tmp/geth -xvf /tmp/geth/geth.tar.gz
+    mkdir -p $BASE_DIR
+    wget https://gethstore.blob.core.windows.net/builds/geth-alltools-linux-$ARCHITECTURE-$GETH_VERSION.tar.gz -O $BASE_DIR/geth.tar.gz
+    tar --directory $BASE_DIR -xvf $BASE_DIR/geth.tar.gz  geth-alltools-linux-$ARCHITECTURE-$GETH_VERSION/swarm
+    rm $BASE_DIR/geth.tar.gz
 fi
 
 # Prepare fixtures with an account.
-DATADIR=/tmp/BZZ/`date +%s`
+DATADIR=$BASE_DIR/BZZ/`date +%s`
 BZZKEY="71c95d6d54292609a62082249bccbebbf79a3f68"
 PASSWORD="password"
 mkdir -p $DATADIR
 cp -R ./test/fixtures/swarm-keystore $DATADIR/keystore
-echo $PASSWORD > /tmp/geth/password
+echo $PASSWORD > $BASE_DIR/password
 
 # Run swarm.
-$BIN_DIR/swarm --bzzaccount $BZZKEY --datadir $DATADIR --password /tmp/geth/password --ens-api '' 2>> $DATADIR/swarm.log &
+$BIN_DIR/swarm --bzzaccount $BZZKEY --datadir $DATADIR --password $BASE_DIR/password --ens-api '' 2>> $DATADIR/swarm.log &
+
+# Wait for the http listener to start; otherwise the test might fail.
+echo "Waiting for the http listener to start..."
+I=10
+while [ $I -gt 0 ]; do
+    nc -z localhost "8500"
+    if [ $? -eq 0 ]; then
+        exit 0;
+    fi
+    sleep 1
+done
+echo "ERROR: The http listener did not start in time."
+exit 1
